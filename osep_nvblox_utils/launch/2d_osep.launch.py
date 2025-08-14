773d435196ff39d6b3d392b74ae0fda2d4731161
KasperMollerHansen <Kasperhansen1998@hotmail.com>
2025-08-14 10:33:01 +0200

    Update 2

--- osep_nvblox_utils/launch/2d_osep.launch.py
+++ osep_nvblox_utils/launch/2d_osep.launch.py
@@ -17,7 +17,7 @@
 
 def generate_launch_description():
     from launch import LaunchDescription
-    from launch_ros.actions import IncludeLaunchDescription
+    from launch.actions import IncludeLaunchDescription
     from launch.launch_description_sources import PythonLaunchDescriptionSource
     from ament_index_python.packages import get_package_share_directory
 
